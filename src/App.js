5c975609111255c5707a1c08f95e1da5a5ff2955
Imre Vigh <vighimre87@gmail.com>
2023-03-15 17:57:43 +0000

    render pages using React router

--- src/App.js
+++ src/App.js
@@ -1,20 +1,29 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css';
 import Header from './components/Header/Header';
 import Contact from './pages/Contact';
 import Home from './pages/Home';
 import Projects from './pages/Projects';
 import Footer from './components/Footer/Footer';
+import Wrapper from './components/Wrapper/Wrapper';
 
 function App() {
   return (
-    <div>
+    <Router>
+      <div>
       <Header />
-      {/* <Home /> */}
-      {/* <Projects /> */}
-      <Contact/>
+      <Wrapper>
+        <Routes>
+          <Route path='/' element={<Home/>} />
+          <Route path='/home' element={<Home/>} />
+          <Route path='/projects' element={<Projects/>} />
+          <Route path='/contact' element={<Contact/>} />
+        </Routes>
+      </Wrapper>
       <Footer />
     </div>
+    </Router>
   );
 }
 
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,5 +1,6 @@
 import React from "react";
 import "./index.css";
+import {Link, NavLink} from 'react-router-dom';
 
 function Header() {
     return (
@@ -7,14 +8,35 @@ function Header() {
     <nav className="nav">
       <div className="nav-header">
         <div className="nav-title">
+          <Link to="/">
           <h1>Imre Vigh</h1>
+          </Link>
         </div>
       </div>
       <div className="nav-links">
-        <a href="#about">About Me</a>
-        <a href="#work">Projects</a>
-        <a href="#contact">Contact Me</a>
-        <a href="files/Resume-Imre-Vigh.pdf" download="">Resume</a>
+        <a className="nav-link" href="#home">
+          <NavLink to='/'
+                    end
+                    className={({isActive}) =>
+                      isActive ? 'nav-link active' : 'nav-link'}>
+            Home
+          </NavLink>
+        </a>
+        <a className="nav-link" href="#projects">
+          <NavLink to='/projects'
+                    end
+                    className={({isActive}) =>
+                      isActive ? 'nav-link active' : 'nav-link'}>
+            Projects
+          </NavLink></a>
+        <a className="nav-link" href="#contact">
+          <NavLink to='/contact'
+                    end
+                    className={({isActive}) =>
+                      isActive ? 'nav-link active' : 'nav-link'}>
+            Contact
+          </NavLink></a>
+        <a className="nav-link" href="files/Resume-Imre-Vigh.pdf" download="">Resume</a>
       </div>
     </nav>
   </header>
