3964c06af620cbabb5ec197e18341a368074d888
Imre Vigh <vighimre87@gmail.com>
2023-03-18 00:12:13 +0000

    added styling to About component

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -9,32 +9,18 @@ function About() {
       </div>
       <div className="vertical-line" id="first-line"></div>
       <div className="sectionContent" id="aboutText">
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras vestibulum sapien vel maximus mollis.
-        Nullam ut ornare ipsum, ut lacinia mauris. In tincidunt vehicula mi, eu ultrices risus lobortis eget.
-        Etiam vestibulum venenatis justo et convallis. Cras aliquet enim nec sapien interdum elementum.
-        Pellentesque auctor nulla id bibendum molestie. Pellentesque vitae tristique ligula, ut bibendum ante.
-        Sed eget suscipit ligula.</p>
-
-        <br />
-
-        <p>Aliquam semper dignissim ligula. Mauris in leo est. Maecenas sagittis dui sed tellus tristique,
-        id suscipit odio laoreet. Vivamus vel odio at dolor dignissim mollis. Vestibulum mollis velit nibh,
-        vitae accumsan eros pulvinar a. Quisque sodales consequat dolor, et sodales arcu scelerisque sed.
-        Proin non leo quis orci blandit aliquam. Aliquam eu nunc auctor quam accumsan pretium vitae id sapien.
-        Phasellus nec dolor ac magna posuere bibendum et vel sem. Sed varius velit id ex iaculis, at maximus orci hendrerit.
-        Nam eget ullamcorper nulla, eget dignissim arcu. Praesent scelerisque accumsan tristique.
-        Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.</p>
-
-        <br />
-
-        <p>Cras venenatis, ipsum a hendrerit condimentum, odio purus euismod dolor, id congue libero lorem et purus.
-        Nulla sed mattis tellus. Nam vitae tortor id nunc dapibus facilisis. Sed vulputate vel metus ac dapibus.
-        Morbi venenatis felis orci, non porttitor dui volutpat mollis. Nam tincidunt urna sed ullamcorper mollis.
-        Quisque quam magna, tincidunt non vulputate vel, condimentum eget leo. Nam a varius arcu.
-        Sed facilisis tellus sit amet sem luctus, eu hendrerit velit pharetra. Curabitur laoreet mauris id vulputate interdum.
-        Etiam vel est pharetra, faucibus turpis non, lacinia enim. Pellentesque viverra pretium maximus.
-        Aliquam accumsan eros a leo posuere, id congue mauris mollis. In at scelerisque purus.
-        Aenean eu dui porttitor, facilisis nunc ac, porttitor nunc.</p>
+        <h2>Hi, I am Imre Vigh 👋</h2>
+        <p>I am an aspiring Frontend Web Developer, currently based in Skipton, UK but I am originally from Hungary.
+          I am recently learning Front Web Development with edX Skills for Life Bootcamp and I am looking forward to
+          use all of my skills to build great websites. I am also a certified Java SE 8 Programmer (OCA Java SE 8 Programmer)
+          and lifelong learner who enjoys working with all types of personalities. I am willing and enthused to resolve all type
+          of challenges in the most efficient manner.</p>
+          <br></br>
+        <p>
+          When I am not working on challenges or websites I like to be outside in the nature. I am an avid walker and I like to
+          challenge myself outside of my work and studies. I also like reading, yoga which is part of my daily routine and solving
+          puzzles or crosswords. And like everyone else, I like to travel and discover different countries and cultures.
+        </p>
       </div>
     </section>
     );
--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -3,12 +3,13 @@ import "./index.css";
 
 function Hero() {
     return (
-        <section class="page-section" id="heroGrid">
+        <section className="page-section" id="heroGrid">
       <div id="hero-image" class="grid-item">
         <img src="https://i.imgur.com/SZLH4uI.jpg" alt="The author in suit" width="200px" height="200px" />
       </div>
       <div id="hero-text" class="grid-item">
-        <h2>A diligent and highly ambitious aspiring software developer who can think outside the box.</h2>
+        <h2>“I have failed many times, and that's why I am a success.”</h2>
+        <p>Michael Jordan (1963 - )</p>
       </div>
   </section>
     );
